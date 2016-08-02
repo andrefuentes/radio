72d73c660e3ed0f0bcd8b575cc220d49af748835
andrefuentes <andrefuentes166@gmail.com>
2016-08-02 14:35:55 -0600

    1ra parte

--- usoradio.py
+++ usoradio.py
@@ -3,26 +3,52 @@ class radio():
 		self.marca=marca
 		self.encendido=False
 		self.en_FM=True
-		self.en_Am=False
 		self.emisora_AM=0
 		self.emisora_FM=88.0
 		self.volumen=0
 	def encender(self):
 		self.encendido=True	
 	def apagar(self):
-		self.encendido=True
+		self.encendido=False
 	def subir_volumen(self):
 		if self.volumen >= 100:
 			self.volumen=100
 		else:
 			self.volumen+=5
 	def bajar_volumen(self):
-		if self.volumen >=0
+		if self.volumen <=0:
 			self.volumen=0
 		else:
 			self.volumen-=5
+				self.emisora_AM+40
 	def subir_emisora(self):
-		if not self.en_Am:
-			if 	self.emisora_AM<=1300
+		if not self.emisora_AM:
+			if self.emisora_FM>=88.0:
+				self.emisora_FM=107.5
+			else:
+				self.emisora_FM+.5
+		else:
+			if not emisora_FM:
+				self.emisora_AM>=1300
 				self.emisora_AM=300
-			elif:
+			else:
+				self.emisora_AM+40
+
+	def bajar_emisora (self):
+		if  self.emisora_AM<=1300:
+			self.emisora_AM=300
+		else:
+			self.emisora_AM-40
+		else:
+			if not self.emisora_FM:
+				self.emisora_FM<=107.5
+				self..emisora_FM=88.0
+			else:
+				self.emisora_FM-.5
+
+
+		
+		
+
+
+
