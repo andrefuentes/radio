b25fe5be2c5cf54e28769845d822079bb429d535
andrefuentes <andrefuentes166@gmail.com>
2016-08-08 14:13:24 -0600

    proceso radio

--- usoradio1.py
+++ usoradio1.py
@@ -2,19 +2,24 @@ from radio import radio
 mi_radio=radio("sony")
 desea_continuar=True
 opc=0
-print("que desea hacer 1.subir_volumen,2.bajar_volumen,3.subir_emisora,4.bajar_emisora,5.")
+print("que desea hacer 1.subir_volumen,2.bajar_volumen,3.subir_emisoraFM,4.bajar_emisoraFM,5.3.subir_emisoraaM,4.bajar_emisoraAM")
 while desea_continuar:
 	if mi_radio.encendido:
 		if opc==1:
-		mi_radio.subir_volumen
+			mi_radio.subir_volumen
 		if opc==2:
-		mi_radio.bajar_volumen
+			mi_radio.bajar_volumen
 		if opc==3:
-		mi_radio.subir_emisoraFM
+			mi_radio.subir_emisoraFM
 		if opc==4:
-		mi_radio.bajar_emisoraFM
+			mi_radio.bajar_emisoraFM
+		if opc==5:
+			mi_radio.bajar_emisoraAM
+		if opc==6:
+			mi_radio.subir_emisoraAm
+
 	else:
-		mi_radio.apagado
-		mi_radio.encender
-		mi_radio.salir
+		print(mi_radio.apagado)
+		print(mi_radio.encender)
+		print(mi_radio.salir)
 
